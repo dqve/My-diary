e2ac0f3e6cc539d4fbe2e1cff69a0492a0d126f9
dqve <ayooladavid@gmail.com>
2021-01-04 00:13:37 +0100

    initial application -5

--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -1,40 +1,51 @@
-import React, {useState, useRef} from 'react';
-import Moment from 'react-moment';
+import React, {useState, useRef, useEffect} from 'react'
+import Moment from 'react-moment'
+
 
 function EntryList({list, deleteEntry, editEntry}) {
-    const handleDeleteClick = (index) => e => {
-      deleteEntry(index);
-    }
     
-    // const [edited, setEdited] = useState("");
-    const [disabled, setDisabled] = useState({});
+    const [disabled, setDisabled] = useState({})
+    const [state, setState] = useState(list)
+    
+    useEffect(() =>(
+        list.map((item, i) => 
+            setDisabled(disabled => ({...disabled, i:false}))
+    )),[list])
     
     const handleEditClick = (index, entry) => e => {
         disabled.index? setDisabled({...disabled, [index]:true}): setDisabled({...disabled, [index]:!disabled.index})
-      // editEntry(index, entry);
+      // editEntry(index, edited)
     }
-    const fieldRef = useRef();
+
+    const handleDeleteClick = (index) => e => {
+        deleteEntry(index)
+    }
+
+    const editDairy = (e) => null
+
+    const fieldRef = useRef()
   
     return (
       <div className="entry-list mt-3">
         {
-          list && list.map((item, i) => {
-            const itemDate = item.date;
+          state && state.map((item, i) => {
+            const itemDate = item.date
             const itemAdded = item.added
-            const flagColor = item.flag ? `bg-${item.flag} text-white` : '';
+            const flagColor = item.flag ? `bg-${item.flag} text-white` : ''
             return (
               <div key={i} className={`card mb-2 ${flagColor}`}>
                 <div className="card-body">
                   <h6 className="card-title">My entry for {itemDate}</h6>
-                  <textarea 
+                  <textarea
                     className="fcard-text"
                     value={item.message}
                     // onChange={handleEdit} 
-                    type="text" 
-                    id="message" 
+                    type="text"
+                    id="message"
                     maxLength={100}
                     width="100%"
                     ref={fieldRef}
+                    onChange={editDairy}
                     disabled={disabled.i?false:true}
                     rows="5"
                     style={{border:"0px", background: 0, color: "white", height:"auto", width: "100%", outline: "none",  resize: "none"}}
@@ -58,6 +69,6 @@ function EntryList({list, deleteEntry, editEntry}) {
         }
       </div>
     )
-  }
+}
 
-  export default EntryList
+export default EntryList
